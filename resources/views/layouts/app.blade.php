443cf6b21d775b47fbcf11cdad7bd9767701e75b
MuhammadSatriap <msatriapp1810@gmail.com>
2024-12-20 15:40:59 +0700

    csspush

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="shortcut icon" href="{{ secure_url(asset('dashboard/images/favicon.png')) }}" />
 
 
@@ -34,7 +35,7 @@
     <div class="container py-5">
         @yield('body')
     </div>
-TEST
+
     <div class="">
         @include('layouts.footer')
     </div>
